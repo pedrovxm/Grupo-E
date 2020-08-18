f0f89e568baafc90cdf7e626e93d47ad1085b06e
Alan Prado <alanctprado@gmail.com>
2020-08-18 01:54:18 -0300

    listar e contar em subpastas

--- scripts_leitura/script_contar_arquivos.py
+++ scripts_leitura/script_contar_arquivos.py
@@ -1,7 +1,12 @@
 import os
 
-def contar_arquivos(dir_name):
-
-    filenames = os.listdir(dir_name)
-
-    return len(filenames)
+def count_files(dir_name):
+    folder_content = os.listdir(dir_name)
+    count = 0
+    for entry in folder_content:
+        entry_path = os.path.join(dir_name, entry)
+        if os.path.isdir(entry_path):
+            count += count_files(entry_path)
+        else:
+            count += 1                
+    return count
--- scripts_leitura/script_listar_diretorio.py
+++ scripts_leitura/script_listar_diretorio.py
@@ -1,7 +1,12 @@
 import os
 
-def listar_diretorio(dir_name):
-
-    filenames = os.listdir(dir_name)
-
-    return filenames
+def get_all_files(dir_name):
+    folder_content = os.listdir(dir_name)
+    all_files = list()
+    for entry in folder_content:
+        entry_path = os.path.join(dir_name, entry)
+        if os.path.isdir(entry_path):
+            all_files = all_files + get_all_files(entry_path)
+        else:
+            all_files.append(entry_path)                
+    return all_files
