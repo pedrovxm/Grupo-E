c4e3ea4e9f5315943283e086145c81b37f7d1ab1
Gabrielnmds <69644988+Gabrielnmds@users.noreply.github.com>
2020-09-08 19:09:48 -0300

    script pra criar XML com os metadados (atualizado)

--- busca_dados/criar_XML_metadados.py
+++ busca_dados/criar_XML_metadados.py
@@ -1,39 +1,29 @@
 import xml.etree.ElementTree as xml
 import pandas as pd
 
-def generate_xml(arquivo):
+def generate_xml(tabela_excel):
 
-    df = pd.read_excel(arquivo)
-    root = xml.Element('Files')
+    df = pd.ExcelFile(tabela_excel)
+    list_sheets = df.sheet_names
+    meta = pd.read_excel(df, list_sheets[0])
+    header = pd.read_excel(df, list_sheets[1])
+
+    root = xml.Element('Arquivos')
     i = 0
-    for row in df.iterrows():
+    for row in meta.iterrows():
         i += 1
-        file = xml.SubElement(root, "ID_Arquivo", name=str(i))
+        arquivo = xml.SubElement(root, "ID_Arquivo", name=str(i))
 
-        identificador = xml.SubElement(file, "Identifidores")
-        xml.SubElement(identificador, 'Nome_Original', name='Nome_Original_Drive').text = str(row[1].iloc[1])
-        xml.SubElement(identificador, 'Descricao', name='Descricao').text = str(row[1].iloc[2])
-        xml.SubElement(identificador, 'Ultima_modificacao', name='Ultima_data_de_modificacao').text = str(row[1].iloc[3])
-        xml.SubElement(identificador, 'Link', name='Link_Arquivo_Drive').text = str(row[1].iloc[13])
-        #tag do novo nome do arquivo
-        #xml.SubElement(identificador, 'Novo_nome', name='Novo_Nome').text =
+        for col in header.iterrrows():
+            nome_XML = col[1].loc['Nome XML']
+            nome_original = str(col[1].loc['Nome original'])
+            xml.SubElement(arquivo, nome_XML, name=nome_original).text = str(row[1].loc[nome_original])
 
-        info_pesq = xml.SubElement(file, 'Informacoes_Pesquisa')
-        xml.SubElement(info_pesq, 'Eixo', name="Eixo").text = eixo[str(row[1].iloc[6])]
-        xml.SubElement(info_pesq, 'Finalidade', name='Finalidade').text = str(row[1].iloc[11])
-        xml.SubElement(info_pesq, 'Local', name='Local da coleta').text = str(row[1].iloc[4])
-        xml.SubElement(info_pesq, 'Fonte', name='Fonte').text = str(row[1].iloc[8])
-
-        info_tec = xml.SubElement(file, "Informacoes_Tecnicas")
-        xml.SubElement(info_tec, "Extracao", name="Tecnica_de_Extracao").text = str(row[1].iloc[10])
-        xml.SubElement(info_tec, "Dado", name="Dado_Estruturado").text = str(row[1].iloc[9])
-        #tag para inserir a extensao do arquivo
-        #xml.SubElement(info_tec, "Extensao", name="Extensao_do_Arquivo").text =
 
     tree = xml.ElementTree(root)
 
     with open("metadados.xml", "wb") as file:
         tree.write(file)
 
-file = "FORMULARIO RESPUESTAS.xlsx"
-generate_xml(file)
+tabela_excel = "coleta de metadados.xlsx"
+generate_xml(tabela_excel)
